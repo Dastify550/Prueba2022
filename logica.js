5c0691ce0b3101c4dc5d4c238138439d0609ac48
Dastify550 <danielfig123456mail.com>
2022-03-19 09:57:40 -0600

    commit

---
+++ logica.js
@@ -0,0 +1,8 @@
+function saves(){
+    let valor = document.getElementById("Esteweon").value;
+	            if(valor==""){
+                alert("No has ingresado nada!")
+                }else{
+                    alert(document.getElementById('AQUIPA').value);
+        }
+}
